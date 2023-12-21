faf9a82b502788b6a0e8ad6492237ae4b3d68c2a
Leonardo Dal Olmo <leo.dalolmoofc@gmail.com>
2023-12-20 21:47:46 -0300

    Criando mensagem de Boa noite

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Bom dia.");
+        System.out.println("Boa noite");
     }
 }
